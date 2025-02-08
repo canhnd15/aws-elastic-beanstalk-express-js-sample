fa5a71d6de9872dde98c3b6d3b76ddbfcff68873
canhnd <canhnd15@gmail.com>
2025-02-08 21:05:41 +0700

    changes 1

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const port = 8080;
 
-app.get("/", (req, res) => res.send("Welcome to DavidNguyen blog!"));
+app.get("/", (req, res) => res.send("Welcome to DavidNguyen blog updated!"));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
